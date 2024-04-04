6e9febadd6aec6badfccadbe00d030647629581a
tharcissentirandekura <tntirand@oberlin.edu>
2024-04-04 16:56:17 -0400

    saave changes

---
+++ web.py
@@ -0,0 +1,11 @@
+from websockets.server import serve
+import asyncio
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+async def main():
+    async with serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
